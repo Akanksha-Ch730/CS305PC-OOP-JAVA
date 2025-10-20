2f8345508df51981a39d1c562087eb83c01d468e
Akanksha-Ch730 <akankshachilvery48@gmail.com>
2025-10-20 18:17:00 +0530

    Add files via upload

---
+++ MINI PROJECT/Stundent main.java
@@ -0,0 +1,34 @@
+//java i.o
+package Student;
+class studentMain implements Operations{
+  int i :String,aname,id,branch,password;
+Scanner sc = new Scanner(System.in)
+	public void register(student std){
+        System.out.println("enter the student id");
+	id = sc.nextLine();
+	std.getid=id;
+	System.out.println("enter student branch");
+	branch=sc.nextLine()
+	std getbranch();
+	System.out.println("enter the student password");
+	password= sc.nextLine();
+	std getPassword(password);
+	System.out.println(std.getDetails);
+	try(
+	Bufferwriter writer = new BufferWriter()
+}
+}
+public void login(String id,String password){
+
+}
+public void exit(){
+}
+public static void main(String...args){
+     System.out.println
+
+
+
+
+
+
+
